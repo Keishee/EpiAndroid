dc1fa4f2d5357bf853b6c1ece4e9fb022df88d0a
Clément <clement.da-silva@epitech.eu>
2016-01-19 17:10:12 +0100

    Added new activity after login in

--- app/src/main/java/com/example/joseph/app/LoginActivity.java
+++ app/src/main/java/com/example/joseph/app/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.joseph.app;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -372,6 +373,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent myIntent = new Intent();
+                myIntent.setClass(getApplicationContext(), FrontPageActivity.class);
+                startActivity(myIntent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
