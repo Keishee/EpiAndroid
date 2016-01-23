079f78992094d954d95006b6fd9c66511b87474e
Joseph Barbisan <joseph.barbisan@outlook.com>
2016-01-23 15:52:53 +0100

    ApiIntra is now static

--- app/src/main/java/com/example/joseph/app/helper/ApiIntra.java
+++ app/src/main/java/com/example/joseph/app/helper/ApiIntra.java
@@ -1,72 +1,96 @@
 package com.example.joseph.app.helper;
 
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 /**
  * Created by Alex on 20/01/2016.
  */
 public class ApiIntra {
 
-    private String _token;
+    private static String _token;
+    private static AppCompatActivity activity;
 
-    public String postToken(String name, String password) {
-        return ApiManager.postApiCall("login", name, "password", password);
+    public static void setActivity(AppCompatActivity act) {
+        activity = act;
     }
 
-    public void setToken(String token) {
+    public static void postToken(String name, String password) {
+        String tmp = ApiManager.postApiCall("login", name, "password", password);
+        JsonParser parser = new JsonParser();
+        JsonObject object = (JsonObject) parser.parse(tmp);
+        _token = object.get("token").getAsString();
+    }
+
+    public static void setToken(String token) {
         _token = token;
     }
 
-    public String getInfos() {
-        return ApiManager.getApiCall("infos", "token", _token);
+    public static void getInfos() {
+        String tmp = ApiManager.getApiCall("infos", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("infos", tmp);
+        editor.commit();
     }
 
     /**
      * "start" : "YYYY-MM-DD"
      * "end" : "YYYY-MM-DD"
-     *
-    **/
-    public String getPlanning(String start, String end) {
-        return ApiManager.getApiCall("planning", "token", _token, start, end);
+     **/
+    public static void getPlanning(String start, String end) {
+        String tmp = ApiManager.getApiCall("planning", "token", _token, start, end);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("planning", tmp);
+        editor.commit();
     }
 
     /**
      * "start" : "YYYY-MM-DD"
      * "end" : "YYYY-MM-DD"
-     *
      **/
-    public String getSusies(String start, String end) {
-        return ApiManager.getApiCall("susies", "token", _token, "start", start, "end", end);
+    public static void getSusies(String start, String end) {
+        String tmp = ApiManager.getApiCall("susies", "token", _token, "start", start, "end", end);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("susies", tmp);
+        editor.commit();
     }
 
     /**
      * "id" : "6301"
      * "calendar_id" : "42"
-     *
-    **/
-    public String getSusie(String id, String calendar_id) {
-        return ApiManager.getApiCall("susies", "token", _token, "id", id, "calendar_id", calendar_id);
-    }
-
-    /**
-    * "id" : "6301"
-    * "calendar_id" : "42"
-     *
      **/
-    public String subscribeSusie(String id, String calendar_id) {
-        return ApiManager.postApiCall("susies", "token", _token, "id", id, "calendar_id", calendar_id);
+    public static void getSusie(String id, String calendar_id) {
+        String tmp = ApiManager.getApiCall("susies", "token", _token, "id", id, "calendar_id", calendar_id);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("susie", tmp);
+        editor.commit();
     }
 
     /**
      * "id" : "6301"
      * "calendar_id" : "42"
-     *
      **/
-    public String unsuscribeSusie(String id, String calendar_id) {
+    public static void subscribeSusie(String id, String calendar_id) {
+        ApiManager.postApiCall("susies", "token", _token, "id", id, "calendar_id", calendar_id);
+    }
+
+    /**
+     * "id" : "6301"
+     * "calendar_id" : "42"
+     **/
+    public static void unsuscribeSusie(String id, String calendar_id) {
         //return ApiManager.postApiCall("susies", "token", _token, "id", id, "calendar_id", calendar_id); ----> DELETE request .. ?
-        return "";
+        String tmp = "";
     }
 
-    public String getProjects() {
-        return ApiManager.getApiCall("projects", "token", _token);
+    public static void getProjects() {
+        String tmp = ApiManager.getApiCall("projects", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("projects", tmp);
+        editor.commit();
     }
 
     /**
@@ -74,9 +98,8 @@ public class ApiIntra {
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1",
      * "codeacti":"acti-167486"
-     *
      **/
-    public String getProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
+    public static String getProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
         return ApiManager.getApiCall("project", "token", _token, "schoolyear", schoolYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi);
     }
 
@@ -85,9 +108,8 @@ public class ApiIntra {
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1",
      * "codeacti":"acti-167486"
-     *
      **/
-    public String subscribeProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
+    public static String subscribeProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
         return ApiManager.postApiCall("project", "token", _token, "schoolyear", schoolYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi);
     }
 
@@ -96,11 +118,10 @@ public class ApiIntra {
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1",
      * "codeacti":"acti-167486"
-     *
      **/
-    public String unsubscribeProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
+    public static void unsubscribeProject(String schoolYear, String codeModule, String codeInstance, String codeActi) {
         //return ApiManager.postApiCall("project", "token", _token, "schoolyear", schoolYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi); ---> DELETE request ?
-        return "";
+        String tmp = "";
     }
 
     /**
@@ -108,33 +129,36 @@ public class ApiIntra {
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1",
      * "codeacti":"acti-167486"
-     *
      **/
-    public String getProjectFiles(String schoolYear, String codeModule, String codeInstance, String codeActi) {
+    public static String getProjectFiles(String schoolYear, String codeModule, String codeInstance, String codeActi) {
         return ApiManager.getApiCall("project/files", "token", _token, "schoolyear", schoolYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi);
     }
 
-    public String getModules() {
-        return ApiManager.getApiCall("modules", "token", _token);
+    public static void getModules() {
+        String tmp = ApiManager.getApiCall("modules", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("modules", tmp);
+        editor.commit();
     }
 
     /**
      * "scolaryear":2014,
      * "location":"FR/PAR" or another location in "FR/BDX","FR/LIL","FR/LYN","FR/MAR","FR/MPL","FR/NCY","FR/NAN","FR/NCE","FR/PAR","FR/REN","FR/STG","FR/TLS"
      * "course":"bachelor/classic" or "bachelor/tek1ed" or "bachelor/tek2ed"
-     *
      */
-    public String getAllModules(String scolarYear, String location, String course) {
-        return ApiManager.getApiCall("allmodules", "token", _token, "scolaryear", scolarYear, "location", location, "course", course);
+    public static void getAllModules(String scolarYear, String location, String course) {
+        String tmp = ApiManager.getApiCall("allmodules", "token", _token, "scolaryear", scolarYear, "location", location, "course", course);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("allModules", tmp);
+        editor.commit();
     }
 
     /**
      * "scolaryear":2014,
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1"
-     *
      */
-    public String getModule(String scolarYear, String codeModule, String codeInstance) {
+    public static String getModule(String scolarYear, String codeModule, String codeInstance) {
         return ApiManager.getApiCall("module", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance);
     }
 
@@ -142,21 +166,19 @@ public class ApiIntra {
      * "scolaryear":2014,
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1"
-     *
      */
-    public String subscribeModule(String scolarYear, String codeModule, String codeInstance) {
-        return ApiManager.postApiCall("module", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance);
+    public static void subscribeModule(String scolarYear, String codeModule, String codeInstance) {
+        ApiManager.postApiCall("module", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance);
     }
 
     /**
      * "scolaryear":2014,
      * "codemodule":"B-GPR-360-0",
      * "codeinstance":"PAR-6-1"
-     *
      */
-    public String unsubscribeModule(String scolarYear, String codeModule, String codeInstance) {
+    public static void unsubscribeModule(String scolarYear, String codeModule, String codeInstance) {
         //return ApiManager.postApiCall("module", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance); ---> DELETE request ?
-        return "";
+        String tmp = "";
     }
 
     /**
@@ -165,9 +187,8 @@ public class ApiIntra {
      * "codeinstance":"NAN-5-1",
      * "codeacti":"acti-164603"
      * "codeevent":"event-173408"
-     *
      */
-    public String getEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
+    public static String getEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
         return ApiManager.getApiCall("event", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent);
     }
 
@@ -177,10 +198,9 @@ public class ApiIntra {
      * "codeinstance":"NAN-5-1",
      * "codeacti":"acti-164603"
      * "codeevent":"event-173408"
-     *
      */
-    public String subscribeEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
-        return ApiManager.postApiCall("event", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent);
+    public static void subscribeEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
+        ApiManager.postApiCall("event", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent);
     }
 
     /**
@@ -189,31 +209,41 @@ public class ApiIntra {
      * "codeinstance":"NAN-5-1",
      * "codeacti":"acti-164603"
      * "codeevent":"event-173408"
-     *
      */
-    public String unsubscribeEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
+    public static void unsubscribeEvent(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent) {
         //return ApiManager.postApiCall("event", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent);
-        return "";
+        String tmp = "";
     }
 
-    public String getMarks() {
-        return ApiManager.getApiCall("marks", "token", _token);
+    public static void getMarks() {
+        String tmp = ApiManager.getApiCall("marks", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("marks", tmp);
+        editor.commit();
     }
 
-    public String getMessages() {
-        return ApiManager.getApiCall("messages", "token", _token);
+    public static void getMessages() {
+        String tmp = ApiManager.getApiCall("messages", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("messages", tmp);
+        editor.commit();
     }
 
-    public String getAlerts() {
-        return ApiManager.getApiCall("alerts", "token", _token);
+    public static void getAlerts() {
+        String tmp = ApiManager.getApiCall("alerts", "token", _token);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("alerts", tmp);
+        editor.commit();
     }
 
     /**
      * "login":"login_x"
-     *
      */
-    public String getPhoto(String login) {
-        return ApiManager.getApiCall("photo", "token", _token, "login", login);
+    public static void getPhoto(String login) {
+        String tmp = ApiManager.getApiCall("photo", "token", _token, "login", login);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("photo", tmp);
+        editor.commit();
     }
 
     /**
@@ -223,10 +253,9 @@ public class ApiIntra {
      * "codeacti":"acti-167486",
      * "codeevent":"event-177013"
      * "tokenvalidationcode":"00000000"
-     *
      */
-    public String valideToken(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent, String token) {
-        return ApiManager.postApiCall("token", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent, "tokenvalidationcode", token);
+    public static void valideToken(String scolarYear, String codeModule, String codeInstance, String codeActi, String codeEvent, String token) {
+        ApiManager.postApiCall("token", "token", _token, "scolaryear", scolarYear, "codemodule", codeModule, "codeinstance", codeInstance, "codeacti", codeActi, "codeevent", codeEvent, "tokenvalidationcode", token);
     }
 
     /**
@@ -235,17 +264,21 @@ public class ApiIntra {
      * "course":"bachelor/classic" or "bachelor/tek1ed" or "bachelor/tek2ed"
      * "promo":"tek3"
      * "offset":43
-     *
      */
-    public String getListStudents(String year, String location, String course, String promo, String offset) {
-        return ApiManager.getApiCall("trombi", "token", _token, "year", year, "location", location, "couse", course, "promo", promo, "offset", offset);
+    public static void getListStudents(String year, String location, String course, String promo, String offset) {
+        String tmp = ApiManager.getApiCall("trombi", "token", _token, "year", year, "location", location, "couse", course, "promo", promo, "offset", offset);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("students", tmp);
+        editor.commit();
     }
 
     /**
      * "login":"login_x"
-     *
      */
-    public String getUser(String login) {
-        return ApiManager.getApiCall("user", "token", _token, "login", login);
+    public static void getUser(String login) {
+        String tmp = ApiManager.getApiCall("user", "token", _token, "login", login);
+        SharedPreferences.Editor editor = activity.getPreferences(activity.MODE_PRIVATE).edit();
+        editor.putString("user", tmp);
+        editor.commit();
     }
 }
