cffbaf85210c6ed6ae582502f5f39055e1d5f5e6
Clément <clement.da-silva@epitech.eu>
2016-01-25 14:20:15 +0100

    removed hyperlinks from messages

--- app/src/main/java/com/example/joseph/app/adapter/messageListViewAdapter.java
+++ app/src/main/java/com/example/joseph/app/adapter/messageListViewAdapter.java
@@ -1,6 +1,10 @@
 package com.example.joseph.app.adapter;
 
 import android.content.Context;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,30 +38,49 @@ public class messageListViewAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView (int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         View vi = convertView;
         if (vi == null)
             vi = inflater.inflate(R.layout.custom_message_row, null);
 
-        JsonObject obj = (JsonObject)jArray.get(position);
+        JsonObject obj = (JsonObject) jArray.get(position);
         String title = obj.get("title").getAsString();
         String content = obj.get("content").getAsString();
         String date = obj.get("date").getAsString();
-        String teacher = ((JsonObject)obj.get("user")).get("title").getAsString();
-        ((TextView)vi.findViewById(R.id.titleView)).setText(title);
-        ((TextView)vi.findViewById(R.id.contentView)).setText(content);
-        ((TextView)vi.findViewById(R.id.teacherView)).setText(teacher);
-        ((TextView)vi.findViewById(R.id.dateView)).setText(date);
+        String teacher = ((JsonObject) obj.get("user")).get("title").getAsString();
+
+        try {
+            title = removeHyperlinkFromString(title);
+            content = removeHyperlinkFromString(content);
+        } catch (Exception e) {}
+
+        ((TextView) vi.findViewById(R.id.titleView)).setText(title);
+        ((TextView) vi.findViewById(R.id.contentView)).setText(content);
+        ((TextView) vi.findViewById(R.id.teacherView)).setText(teacher);
+        ((TextView) vi.findViewById(R.id.dateView)).setText(date);
         return vi;
     }
 
+    private String removeHyperlinkFromString(String string) {
+        while (string.contains("<a href")) {
+            String linkOpen = string.substring(string.indexOf("<"), string.indexOf(">") + 1);
+            string = string.replace(linkOpen, "$OPEN$");
+            String hyperlink = string.substring(string.indexOf("$OPEN$") + 6, string.indexOf("<"));
+            string = string.replace(hyperlink, "$LINK$");
+            String linkClose = string.substring(string.indexOf("</"), string.indexOf(">") + 1);
+            string = string.replace(linkClose, "$CLOSE$");
+            string = string.replace("$OPEN$$LINK$$CLOSE$", hyperlink);
+        }
+        return string;
+    }
+
     @Override
-    public long getItemId (int position) {
+    public long getItemId(int position) {
         return 0;
     }
 
     @Override
-    public Object getItem (int position) {
+    public Object getItem(int position) {
         return null;
     }
 }
