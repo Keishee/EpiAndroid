4fda8d3d1c0039895221a9ecf7f7c2fa004876dd
Clément <clement.da-silva@epitech.eu>
2016-01-26 15:22:06 +0100

    removed useless comments and dummy fields

--- app/src/main/java/com/example/joseph/app/TrombiFragment.java
+++ app/src/main/java/com/example/joseph/app/TrombiFragment.java
@@ -18,15 +18,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class TrombiFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     public TrombiFragment() {
@@ -37,16 +28,11 @@ public class TrombiFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment TrombiFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static TrombiFragment newInstance(String param1, String param2) {
         TrombiFragment fragment = new TrombiFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,10 +40,6 @@ public class TrombiFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -65,13 +47,6 @@ public class TrombiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trombi, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -102,7 +77,6 @@ public class TrombiFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
