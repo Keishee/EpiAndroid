c605c3df6d5f9e28f6d58daca351af389332b2e5
Clément <clement.da-silva@epitech.eu>
2016-01-26 13:46:37 +0100

    Date as real "Dat" in planning info, room in planning tab & user friendlier date in planning

--- app/src/main/java/com/example/joseph/app/helper/PlanningInfo.java
+++ app/src/main/java/com/example/joseph/app/helper/PlanningInfo.java
@@ -1,15 +1,23 @@
 package com.example.joseph.app.helper;
 
+import java.text.DateFormat;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Clément on 25/01/2016.
  */
 public class PlanningInfo {
-    public String title;
-    public String module;
-    public int semester;
-    public String room;
-    public String start;
-    public String end;
+    private String title;
+    private String module;
+    private int semester;
+    private String room;
+    private String start;
+    private String end;
+    private Date startDate;
+    private Date endDate;
+
     boolean registered;
 
     // Todo: stock start/end as Date?
@@ -21,6 +29,33 @@ public class PlanningInfo {
         this.start = start;
         this.end = end;
         this.registered = registered;
+        ParsePosition pos = new ParsePosition(0);
+        DateFormat simpledateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        startDate = simpledateformat.parse(start, pos);
+        pos.setIndex(0);
+        endDate = simpledateformat.parse(end, pos);
+    }
+
+    public String getRoom() {
+        return getRoom(room);
+    }
+
+    public String getNameOfDay() {
+        SimpleDateFormat outFormat = new SimpleDateFormat("EEEE");
+        return outFormat.format(startDate);     }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public Date getDuration() {
+        long diff = endDate.getTime() - startDate.getTime();
+        Date duration = new Date(diff);
+        return duration;
     }
 
     public boolean isRegistered() {
@@ -28,7 +63,7 @@ public class PlanningInfo {
     }
 
     public String getRoom(String fullRoom) {
-        return fullRoom.substring(room.lastIndexOf("/"));
+        return fullRoom.substring(room.lastIndexOf("/") + 1);
     }
 
     public String getEnd() {
